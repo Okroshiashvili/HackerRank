471d05d87bf1e572f3f3197e6a3843b525872237
Nodo Okroshiashvili <n.okroshiashvili@gmail.com>
2018-03-23 17:48:59 +0400

    Add files via upload

---
+++ Hacker Rank Solutions/Day 1 Quartiles.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 13:25:28 2018
+
+@author: Nodar.Okroshiashvili
+"""
+
+n = int(input().strip())
+X = list(map(int, input().strip().split(' ')))
+X = sorted(X)
+
+
+def quartiles(X): 
+    median = (X[n // 2] + X[-(n // 2 + 1)]) // 2
+    if n % 2 == 0:
+        L = X[: n // 2]
+        U = X[n // 2 :]
+    else:
+        L = X[:X.index(median)]
+        U = X[X.index(median)+1:]
+        
+    Q1 = (L[len(L) // 2] + L[-(len(L) // 2 + 1)]) // 2
+    Q2 = median
+    Q3 = (U[len(U) // 2] + U[-(len(U) // 2 + 1)]) // 2
+    print(Q1, Q2, Q3, sep='\n')
+
+
+quartiles(X)
+
+
+
+
+
+
+
+
