9f95a7670512235b7b7cfc56b4988a5ba8c8a0fc
Nodo Okroshiashvili <n.okroshiashvili@gmail.com>
2018-03-29 17:44:40 +0400

    Add files via upload

---
+++ 10 Days of Statistics/Day 0 weighted Mean.py
@@ -0,0 +1,17 @@
+"""
+Created on Fri Mar 23 12:51:39 2018
+
+@author: Nodar.Okroshiashvili
+"""
+
+
+N = int(input().strip())
+X = list(map(int, input().strip().split(' ')))
+W = list(map(int, input().strip().split(' ')))
+
+def weighted_mean(X, W):
+    output = round(sum([i * j for i, j in zip(X,W)]) / sum(W),1)
+    print(output)
+
+weighted_mean(X,W)
+
