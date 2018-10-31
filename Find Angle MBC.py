2aeee6f4cdc796b55caba1aa9646ffad21406e87
Nodo Okroshiashvili <n.okroshiashvili@gmail.com>
2018-10-31 16:59:35 +0400

    Add files via upload

---
+++ Find Angle MBC.py
@@ -0,0 +1,14 @@
+"""
+Created on Wed Oct 31 10:26:08 2018
+
+@author: nokroshiashvili
+"""
+
+import math
+AB = input()
+BC = input()
+
+print(str(int((round((math.degrees(math.atan2(AB,BC)))))))+'°')
+
+
+
