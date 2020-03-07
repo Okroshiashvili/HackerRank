3ef89799e2b55ddea13448ef8209c1c764e1804b
Okroshiashvili <n.okroshiashvili@gmail.com>
2020-03-07 18:15:00 +0400

    Add Regex folder

---
+++ Regex/Matching Specific String.py
@@ -0,0 +1,14 @@
+
+
+# Case sensitive search
+Regex_Pattern = r'hackerrank'
+
+import re
+
+Test_String = input()
+
+match = re.findall(Regex_Pattern, Test_String)
+
+print("Number of matches :", len(match))
+
+
